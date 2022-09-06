8d3c5ada11fd11e2c9db215586567331740cdbe2
Karl Tayeb <karl.tayeb7@gmail.com>
2022-09-06 11:29:09 -0400

    working mococomo! but slow

--- R/mococomo.R
+++ R/mococomo.R
@@ -22,9 +22,10 @@
 
 
 #' Predict to log pi
-#' Convert K-1 log odds to a K vector of log probabilities
+#' Computes E[log p(z | \beta, \omega)] from E[Xb_1], ... E[Xb_{K-1}]
+#' Where expectations are taken under variational approximation
 #' @param xb vector, length K-1 of conditional log odds for first K-1 components
-#' @return a vector of length K of prior assignment probabilities on log scale
+#' @return a vector of length K of (expected) log prior assignment probabilities
 #' @example
 #' exp(.predict2logpi(rep(0, 3)))  # = c(0.5, 0.25, 0.25)
 .predict2logpi <- function(xb){
@@ -42,26 +43,39 @@
 #' For each data point return covariate-dependent prior mixture probabilities
 #' @param fit a MoCoCoMo fit object
 #' @return an n x K matrix of log prior probabilities for each data point
-
 .compute_prior_assignment <- function(xb){
   .predict2logpi(xb)
 }
+
+
+#' Get N x K-1 matrix of PG variational parameters
+#' TODO: deprecate this when we call update functions directly rather than
+#' passing to logistic susie subroutine
+get_xi.mococomo <- function(fit){
+  xi <- do.call(cbind, purrr::map(fit$logreg_list, ~purrr::pluck(.x, 'params', 'xi')))  # N x K-1
+  return(xi)
+}
+
+compute_Xb.mococomo <- function(fit){
+  Xb <- do.call(cbind, purrr::map(fit$logreg_list, compute_Xb.binsusie))  # N x K-1
+  return(Xb)
+}
+
+compute_prior_assignment <- function(fit){
+  # TODO alias compute_Xb with predict so that it works with other functions?
+  # TODO make sure GLM predict outputs log-odds scale?
+  Xb <- do.call(cbind, purrr::map(fit$logreg_list, compute_Xb.binsusie))  # N x K-1
+  res <- do.call(rbind, apply(Xb, 1, .predict2logpi, simplify = F))  # N x K
+  return(res)
+}
+
 
 .compute_prior_assignment2 <- function(xb){
   log(.pi_tilde2pi(sigmoid(xb)))
 }
 
-compute_prior_assignment <- function(fit, log=TRUE){
-  # TODO alias compute_Xb with predict so that it works with other functions?
-  # TODO make sure GLM predict outputs log-odds scale?
-  Xb <- do.call(cbind, purrr::map(fit$logreg_list, compute_Xb.binsusie))  # N x K-1
-  res <- do.call(rbind, apply(Xb, 1, .predict2logpi, simplify = F))  # N x K
-
-  if(!log){res <- exp(res)}  # exponentiate if log=FALSE
-  return(res)
-}
-
-
+#' DONT USE, this ignores the PG agumentation and treats E[Xb_k] as the conditional log odds of sampling component k
+#' and simply converts these quantitities to a vector of probabilities
 compute_prior_assignment2 <- function(fit, log=TRUE){
   # TODO alias compute_Xb with predict so that it works with other functions?
   # TODO make sure GLM predict outputs log-odds scale?
@@ -85,34 +99,129 @@ compute_data_loglikelihood <- function(fit){
   return(data_loglik)
 }
 
+
+.asignment_loglik <- function(xb, xb2){
+  K <- length(xb) + 1
+  xbcum <- cumsum(xb)
+  kln2 <- seq(K - 1) * log(2)
+  logpi <- -kln2 + xb - 0.5 * xbcum  # un-normalized k=1, ..., K-1
+  logpiK <- -tail(kln2, 1) - 0.5 * tail(xbcum, 1)  # for component K
+  logpi <- c(logpi, logpiK)
+  logpi <- logpi - logSumExp(logpi)
+  return(logpi)
+}
+
+#' Compute Expected Assignment Log Likelihood
+#' @return N x K matrix each row has E[p(z=k | \beta, \omega)] for k = 1,..., K
+compute_assignment_loglikelihood.mococomo <- function(fit, normalize=TRUE){
+  K <- length(fit$f_list)
+  Xb <- do.call(cbind, purrr::map(fit$logreg_list, compute_Xb.binsusie))  # N x K-1
+  Xbcum <- do.call(rbind, apply(Xb, 1, cumsum, simplify = F))
+  kln2 <- seq(K-1) * log(2)
+
+  # second moments only appear in normalizing constant
+  C <- 0
+  if(normalize){
+    Xb2 <- do.call(cbind, purrr::map(fit$logreg_list, compute_Xb2.binsusie))  # N x K - 1
+    omega <- do.call(cbind, purrr::map(fit$logreg_list, compute_omega))  # N x K-1
+    C <- -0.5 * rowSums(Xb2 * omega)
+  }
+
+  logp <- Xb - 0.5 * Xbcum  + C
+  logp <- do.call(rbind, apply(logp, 1, function(x) x - kln2, simplify = F))
+
+  logpK <- -(K-1) * log(2) - 0.5 * Xbcum[, K-1] + C
+  logp <- cbind(logp, logpK)
+  return(logp)
+}
+
+
+
+compute_assignment_jj_bound.mococomo <- function(fit){
+  K <- length(fit$f_list)
+  Xb <- do.call(cbind, purrr::map(fit$logreg_list, compute_Xb.binsusie))  # N x K-1
+  Xi <- do.call(cbind, purrr::map(fit$logreg_list, ~purrr::pluck(.x, 'params', 'xi')))  # N x K-1
+
+  f <- function(xi, xb){
+    tmp <- cumsum(log(sigmoid(xi)) - 0.5 * xi - 0.5 * xb) + xb
+    tmpK <- sum(log(sigmoid(xi)) - 0.5 * xi - 0.5 * xb)
+    jj <- c(tmp, tmpK)
+    return(jj)
+  }
+  jj <- do.call(rbind, purrr::map(seq(nrow(Xb)), ~ f(Xi[.x,], Xb[.x,])))
+
+  # JJ bound, should be faster with matrix operations?
+  # tmp <- rowCumSum(log(sigmoid(xi)) - 0.5 * xi - 0.5 * Xb)
+  # jjK <- tmp[, K-1]
+  # jj <- cbind(Xb + tmp, jjK)
+  return(jj)
+}
+
 #' Compute Posterior Assignment Probabilities
 #' For each data point return posterior assignment probabilities
 #' @param fit a MoCoCoMo fit object
 #' @return an n x K matrix of log posterior probabilities for each data point
 compute_posterior_assignment <- function(fit, log=TRUE){
-  logpi <- compute_prior_assignment(fit, log=TRUE)
   data_loglik <- fit$data_loglik
+  assignment_loglik <- compute_assignment_jj_bound.mococomo(fit)
+  # assignment_loglik <- compute_assignment_loglikelihood.mococomo(fit)
 
   # normalize
   res <- do.call(
     rbind,
-    apply(data_loglik + logpi, 1, function(x) x - logSumExp(x), simplify = F)
+    apply(data_loglik + assignment_loglik, 1, function(x) x - logSumExp(x), simplify = F)
   )
-
   if(!log){res <- exp(res)}  # exponentiate if log=FALSE
   return(res)
 }
 
 
+compute_elbo3.mococomo <- function(fit){
+  post_assignment <- fit$post_assignment
+  data_loglik <- fit$data_loglik
+  jj <- compute_assignment_jj_bound.mococomo(fit)
+
+  kl_susie <- sum(purrr::map_dbl(fit$logreg_list, compute_kl.binsusie))
+  assignment_entropy <- sum(apply(post_assignment, 1, categorical_entropy))
+
+  elbo <- sum(post_assignment * (jj + data_loglik)) + assignment_entropy - kl_susie
+  return(elbo)
+}
+
+compute_elbo2.mococomo <- function(fit){
+  post_assignment <- fit$post_assignment
+  assignment_loglik <- compute_assignment_loglikelihood.mococomo(fit)
+  ll <- sum(post_assignment * (fit$data_loglik + assignment_loglik))
+  assignment_entropy <- sum(apply(post_assignment, 1, categorical_entropy))
+
+  kl_susie <- sum(purrr::map_dbl(fit$logreg_list, compute_kl.binsusie))
+  kl_omega <- sum(purrr::map_dbl(fit$logreg_list, function(x) sum(pg_kl(x$data$N, x$params$xi))))
+  kl <- kl_susie + kl_omega
+
+  return (ll + assignment_entropy - kl)
+}
+
 compute_elbo.mococomo <- function(fit){
+  K <- length(fit$f_list)
+  N <- .expected_trials(fit$post_assignment)
+  # MAKE SURE susie has access to the right posteriors!
+  # and update omega so jj bound is tight
+  for(k in seq(K-1)){
+    fit$logreg_list[[k]]$data$y <- fit$post_assignment[, k]
+    fit$logreg_list[[k]]$data$N <- N[, k]
+    fit$logreg_list[[k]]$params$xi <- update_xi.binsusie(fit$logreg_list[[k]])
+    fit$logreg_list[[k]]$params$tau <- compute_tau(fit$logreg_list[[k]])
+  }
+
+
   # TODO: do we want to just store post_assignment after each update?
   post_assignment <- fit$post_assignment
   data_loglik <- fit$data_loglik
 
   ll <- sum(post_assignment * data_loglik)
   assignment_entropy <- sum(apply(post_assignment, 1, categorical_entropy))
-  logreg_kl <- sum(purrr::map_dbl(fit$logreg_list, compute_elbo.binsusie))
-  elbo <- ll + assignment_entropy + logreg_kl
+  logreg_elbo <- sum(purrr::map_dbl(fit$logreg_list, compute_elbo.binsusie))
+  elbo <- ll + assignment_entropy + logreg_elbo
   return(elbo)
 }
 
@@ -138,6 +247,9 @@ init.mococomo <- function(data){
   # initialize posterior assignment
   Y <- matrix(rep(1, K*n)/K, nrow=n)
   N <- .expected_trials(Y)
+
+  data$y <- Y
+  data$N <- N
 
   # initialize K-1 logistic SuSiE
   .init_logreg <- function(y, N){
@@ -178,6 +290,7 @@ iter.mococomo <- function(fit, update_assignment=T, update_logreg=T){
     logreg_list <- list()
     for(k in seq(K-1)){
       logreg <- fit$logreg_list[[k]]
+
       # pass assignments to logreg
       logreg$data$y <- fit$post_assignment[, k]
       logreg$data$N <- fit$N[, k]
@@ -200,9 +313,9 @@ fit.mococomo <- function(data, maxiter=100, tol=1e-3){
   fit$elbo <- compute_elbo.mococomo(fit)
   for(i in 1:maxiter){
     fit <- iter.mococomo(fit, is.even(i), is.odd(i))
-    fit$elbo <- c(fit$elbo, compute_elbo.mococomo(fit))
+    fit$elbo <- c(fit$elbo, compute_elbo3.mococomo(fit))
 
-    print(paste('asgn:', is.even(i), 'logreg:', is.odd(i), 'elbo: ', tail(fit$elbo, 1)))
+    # print(paste('asgn:', is.even(i), 'logreg:', is.odd(i), 'elbo: ', tail(fit$elbo, 1)))
     if (.converged(fit, tol)){
       break
     }
--- tests/testthat/test-jj_bound.R
+++ tests/testthat/test-jj_bound.R
@@ -12,3 +12,62 @@ testthat::test_that("Explicit ELBO and  JJ bound agree", {
   b <- explicit_elbo.binser(fit)
   testthat::expect_equal(a, b)
 })
+
+
+testthat::test_that('Mococomo bound can be computed from SuSiE bounds', {
+  # confirm that
+  # \sum_k(JJ + KL(omega)) = E[logp(y | \beta, \omega)]
+  data <- sim_mococomo()
+  fit <- fit.mococomo(data, maxiter = 5)
+
+  K <- length(fit$f_list)
+  N <- .expected_trials(fit$post_assignment)
+  # update omega so jj bound is tight
+  for(k in seq(K-1)){
+    fit$logreg_list[[k]]$data$y <- fit$post_assignment[, k]
+    fit$logreg_list[[k]]$data$N <- N[, k]
+    fit$logreg_list[[k]]$params$xi <- update_xi.binsusie(fit$logreg_list[[k]])
+    fit$logreg_list[[k]]$params$tau <- compute_tau(fit$logreg_list[[k]])
+  }
+
+
+  logp <- compute_assignment_loglikelihood.mococomo(fit)
+  post_assignment <- fit$post_assignment
+  a <- rowSums(logp * post_assignment)
+
+  # compute E[logp(z | beta, omega)] as sum of jj bounds
+  jj <- do.call(cbind, purrr::map(fit$logreg_list, jj_bound.binsusie))
+  omega_kl <- do.call(cbind, purrr::map(fit$logreg_list, function(x) pg_kl(x$data$N, x$params$xi)))
+  b <- rowSums(jj + omega_kl)
+
+  testthat::expect_equal(a, b)
+})
+
+
+testthat::test_that('Mococomo bound can be computed from SuSiE bounds 2', {
+  # confirm that
+  # \sum_k(JJ + KL(omega)) = E[logp(y | \beta, \omega)]
+  data <- sim_mococomo()
+  fit <- fit.mococomo(data, maxiter = 5)
+
+  K <- length(fit$f_list)
+  N <- .expected_trials(fit$post_assignment)
+  # update omega so jj bound is tight
+  for(k in seq(K-1)){
+    fit$logreg_list[[k]]$data$y <- fit$post_assignment[, k]
+    fit$logreg_list[[k]]$data$N <- N[, k]
+    fit$logreg_list[[k]]$params$xi <- update_xi.binsusie(fit$logreg_list[[k]])
+    fit$logreg_list[[k]]$params$tau <- compute_tau(fit$logreg_list[[k]])
+  }
+
+
+  logp <- compute_assignment_jj_bound.mococomo(fit)
+  post_assignment <- fit$post_assignment
+  a <- rowSums(logp * post_assignment)
+
+  # compute E[logp(z | beta, omega)] as sum of jj bounds
+  jj <- do.call(cbind, purrr::map(fit$logreg_list, jj_bound.binsusie))
+  b <- rowSums(jj)
+
+  testthat::expect_equal(a, b)
+})
