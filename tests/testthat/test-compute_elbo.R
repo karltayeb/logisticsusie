2ba516194a2dfde212dffc7e8c96759e136e4336
Karl Tayeb <karl.tayeb7@gmail.com>
2022-09-07 18:25:56 -0400

    cleanup tests

--- tests/testthat/test-compute_elbo.R
+++ tests/testthat/test-compute_elbo.R
@@ -1,8 +1,3 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-
 testthat::test_that("Different MoCoCoMo ELBO functions agree", {
   data <- sim_mococomo()
   fit <- fit.mococomo(data, maxiter = 10)
@@ -23,30 +18,6 @@ testthat::test_that("Different MoCoCoMo ELBO functions agree", {
   testthat::expect_equal(elbo1, elbo2)
   testthat::expect_equal(elbo1, elbo3)
 
-})
-
-
-
-testthat::test_that("Different MoCoCoMo ELBO functions agree", {
-  data <- sim_mococomo()
-  fit <- fit.mococomo(data, maxiter = 10)
-
-  K <- length(fit$f_list)
-  N <- .expected_trials(fit$post_assignment)
-
-  # update omega so jj bound is tight
-  for(k in seq(K-1)){
-    fit$logreg_list[[k]]$data$y <- fit$post_assignment[, k]
-    fit$logreg_list[[k]]$data$N <- N[, k]
-    fit$logreg_list[[k]]$params$xi <- update_xi.binsusie(fit$logreg_list[[k]])
-    fit$logreg_list[[k]]$params$tau <- compute_tau(fit$logreg_list[[k]])
-  }
-
-  JJ1 <- compute_assignment_jj_bound.mococomo(fit) * fit$post_assignment
-  JJ2 <- do.call(cbind, purrr::map(fit$logreg_list, jj_bound.binsusie))
-  a <- rowSums(JJ1)
-  b <- rowSums(JJ2)
-  testthat::expect_equal(a, b)
 })
 
 
--- tests/testthat/test-jj_bound.R
+++ tests/testthat/test-jj_bound.R
@@ -14,14 +14,13 @@ testthat::test_that("Explicit ELBO and  JJ bound agree", {
 })
 
 
-testthat::test_that('Mococomo bound can be computed from SuSiE bounds', {
-  # confirm that
-  # \sum_k(JJ + KL(omega)) = E[logp(y | \beta, \omega)]
+testthat::test_that("MOCOMO JJ = sum(Bin SuSiE JJ)", {
   data <- sim_mococomo()
-  fit <- fit.mococomo(data, maxiter = 5)
+  fit <- fit.mococomo(data, maxiter = 10)
 
   K <- length(fit$f_list)
   N <- .expected_trials(fit$post_assignment)
+
   # update omega so jj bound is tight
   for(k in seq(K-1)){
     fit$logreg_list[[k]]$data$y <- fit$post_assignment[, k]
@@ -30,44 +29,7 @@ testthat::test_that('Mococomo bound can be computed from SuSiE bounds', {
     fit$logreg_list[[k]]$params$tau <- compute_tau(fit$logreg_list[[k]])
   }
 
-
-  logp <- compute_assignment_loglikelihood.mococomo(fit)
-  post_assignment <- fit$post_assignment
-  a <- rowSums(logp * post_assignment)
-
-  # compute E[logp(z | beta, omega)] as sum of jj bounds
-  jj <- do.call(cbind, purrr::map(fit$logreg_list, jj_bound.binsusie))
-  omega_kl <- do.call(cbind, purrr::map(fit$logreg_list, function(x) pg_kl(x$data$N, x$params$xi)))
-  b <- rowSums(jj + omega_kl)
-
-  testthat::expect_equal(a, b)
-})
-
-
-testthat::test_that('Mococomo bound can be computed from SuSiE bounds 2', {
-  # confirm that
-  # \sum_k(JJ + KL(omega)) = E[logp(y | \beta, \omega)]
-  data <- sim_mococomo()
-  fit <- fit.mococomo(data, maxiter = 5)
-
-  K <- length(fit$f_list)
-  N <- .expected_trials(fit$post_assignment)
-  # update omega so jj bound is tight
-  for(k in seq(K-1)){
-    fit$logreg_list[[k]]$data$y <- fit$post_assignment[, k]
-    fit$logreg_list[[k]]$data$N <- N[, k]
-    fit$logreg_list[[k]]$params$xi <- update_xi.binsusie(fit$logreg_list[[k]])
-    fit$logreg_list[[k]]$params$tau <- compute_tau(fit$logreg_list[[k]])
-  }
-
-
-  logp <- compute_assignment_jj_bound.mococomo(fit)
-  post_assignment <- fit$post_assignment
-  a <- rowSums(logp * post_assignment)
-
-  # compute E[logp(z | beta, omega)] as sum of jj bounds
-  jj <- do.call(cbind, purrr::map(fit$logreg_list, jj_bound.binsusie))
-  b <- rowSums(jj)
-
-  testthat::expect_equal(a, b)
+  jj1 <- rowSums(compute_assignment_jj_bound.mococomo(fit) * fit$post_assignment)
+  jj2 <- rowSums(do.call(cbind, purrr::map(fit$logreg_list, jj_bound.binsusie)))
+  testthat::expect_equal(jj1, jj2)
 })
