cca4ef4cad60b24b954908ca0131b5d47c68a949
Karl Tayeb <karl.tayeb7@gmail.com>
2022-09-08 08:20:02 -0400

    binsusie driver function that matches susie interface

--- R/binomial_susie.R
+++ R/binomial_susie.R
@@ -227,5 +227,74 @@ fit.binsusie <- function(
   return(fit)
 }
 
+#' @param X a n x p matrix of covariates
+#' @param y an n vector of integer counts, bernoulli/binomial observations
+#' @param N the number of binomial trials, defaults to 1, may be a scalar or vector of length n
+#' @param Z fixed effect covaraites (including intercept). If null just a n x 1 matrix of ones
+#' @param prior_mean the prior mean of each non-zero element of b. Either a scalar or vector of length L.
+#' @param prior_variance the prior variance of each non-zero element of b. Either a scalar or vector of length L. If `estimate_prior_variance=TRUE` the value provides an initial estimate of prior variances
+#' @param prior_weights prior probability of selecting each column of X, vector of length p summing to one, or an L x p matrix
+#' @param intercept
+#' @param estimate_prior_variance
+#' @param s_init a logistic susie object to initialize with, NOTE if non-null, we ignore `prior_mean`, `prior_variance`, and `prior_weights`
+#' @export
+binsusie <- function(
+  X,
+  y,
+  N = 1,  # number of trials for each
+  Z=NULL,
+  L = min(10, ncol(X)),
+  prior_mean = 0.0, # set prior mean (feature added for Gao and Bohan)
+  prior_variance = 1.0,  # TODO: make scaled prior variance?
+  prior_weights = NULL, # vector of length `p` gjvj g the prior probability of each column of X having nonzero effect... = hypers$pi
+  #null_weight = NULL,
+  #standardize = TRUE,
+  intercept = TRUE,
+  estimate_prior_variance = TRUE,
+  #estimate_prior_method = c("optim", "EM", "simple"),  right now only EM is implimented
+  #check_null_threshold = 0,
+  #prior_tol = 1e-09,
+  s_init = NULL, # previous fit with which to initialize NO
+  coverage = 0.95,
+  min_abs_corr = 0.5,
+  max_iter = 100,
+  tol = 0.001,
+  verbose = FALSE,
+  #track_fit = FALSE,
+  #refine = FALSE,
+  n_purity = 100
+){
+  ##################
+  # checking / setup
 
+  # Make data list
+  data <- list(X = X, Z=Z, y=y, N=N)
+
+  # Initialize object
+  if(is.null(s_init)){
+    fit <- init.binsusie(
+      data,
+      L=L,
+      prior_mean = prior_mean,
+      prior_variance = prior_variance,
+      prior_weights=prior_weights
+    )
+  } else{
+    fit <- s_init
+  }
+
+  # model fitting
+  for(i in 1:max_iter){
+    fit <- iter.binsusie(fit, intercept, estimate_prior_variance)
+    fit$elbo <- c(fit$elbo, compute_elbo.binsusie(fit))
+    if (.converged(fit, tol)){
+      break
+    }
+  }
+
+  # post-processing
+
+  # 1. compute credible sets
+  return(fit)
+}
 
