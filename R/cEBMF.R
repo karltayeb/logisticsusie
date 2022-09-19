9b9ac066ea75e7060817f7f578fed09a17bbe31d
william-denault <william.denault@gmail.com>
2022-09-19 20:43:29 +0200

    basic cEBMF working

--- R/cEBMF.R
+++ R/cEBMF.R
@@ -1,4 +1,6 @@
-
+#### TODO
+#ELBO
+#Remove factor which prior is null
 
 #' @title Initialize cEBMF object
 #' @description  Initialize cEBMF object
@@ -8,68 +10,119 @@
 #' @param K numeric number of factors
 #'  @param type_noise specify which kind of noise structure is expected, currently three choices. Whether noise constant accross column ('column_wise'), constant 'constant' or constant across rown 'row_wise'
 #' @return a cEBMF object
-init_cEBMF <- function(Y, X_l,X_f,K=1, type_noise='column_wise' )
+init_cEBMF <- function(Y, X_l,X_f,K=1, type_noise='constant',init_type="udv_si" )
 {
-  cEBMF.obj <- list(
-    Y          = Y,
-    Y_fit      = 0*Y,
-    loading    = matrix(1,nrow=nrow(Y), ncol=K), #first moment
-    factor     = matrix(1,nrow=ncol(Y), ncol=K),
-    loading2   = matrix(1,nrow=nrow(Y), ncol=K),#second moment
-    factor2    = matrix( 1,nrow=ncol(Y), ncol=K),
-    tau        = matrix(1, ncol = ncol(Y), nrow=nrow(Y)),
-    X_l        = X_l,
-    X_f        = X_f,
-    K          = K,
-    type_noise = type_noise
-  )
+  if (init_type=="ones"){
+    cEBMF.obj <- list(
+      Y          = Y,
+      Y_fit      = 0*Y,
+      loading    = matrix(1,nrow=nrow(Y), ncol=K), #first moment
+      factor     = matrix(1,nrow=ncol(Y), ncol=K),
+      loading2   = matrix(1,nrow=nrow(Y), ncol=K),#second moment
+      factor2    = matrix( 1,nrow=ncol(Y), ncol=K),
+      tau        = matrix(1, ncol = ncol(Y), nrow=nrow(Y)),
+      X_l        = X_l,
+      X_f        = X_f,
+      K          = K,
+      type_noise = type_noise
+    )
+  }
+ if(init_type=="rand_udv"){
+   s <- svd( matrix(rnorm(prod(dim(Y))),
+                    ncol= ncol(Y)
+                    ), K,K
+   )
+   l <- s$u[,1:K]%*%diag(s$d[1:K])
+   f <- s$v[,1:K]
+   cEBMF.obj <- list(
+     Y          = Y,
+     Y_fit      = 0*Y,
+     loading    = l  , #first moment
+     factor     = f,
+     loading2   = l^2,#second moment
+     factor2    = f^2,
+     tau        = matrix(1, ncol = ncol(Y), nrow=nrow(Y)),
+     X_l        = X_l,
+     X_f        = X_f,
+     K          = K,
+     type_noise = type_noise
+   )
+ }
+  if(init_type=="udv"){
 
-
+    s <- svd(Y, K,K)
+    l <- s$u[,1:K]%*%diag(s$d[1:K])
+    f <- s$v[,1:K]
+    cEBMF.obj <- list(
+      Y          = Y,
+      Y_fit      = 0*Y,
+      loading    = l  , #first moment
+      factor     = f,
+      loading2   = l ^2,#second moment
+      factor2    = f ^2,
+      tau        = matrix(1, ncol = ncol(Y), nrow=nrow(Y)),
+      X_l        = X_l,
+      X_f        = X_f,
+      K          = K,
+      type_noise = type_noise
+    )
+  }
+  if(init_type=="udv_si"){
+    suppressWarnings(
+    s <- softImpute(Y, rank.max=K,type="als", lambda=0)
+    )
+    l <-  s$u[,1:K]%*%diag(s$d[1:K])
+    f <- s$v[,1:K]
+    cEBMF.obj <- list(
+      Y          = Y,
+      Y_fit      = 0*Y,
+      loading    = l , #first moment
+      factor     = f,
+      loading2   = l^2,#second moment
+      factor2    = f^2,
+      tau        = matrix(1, ncol = ncol(Y), nrow=nrow(Y)),
+      X_l        = X_l,
+      X_f        = X_f,
+      K          = K,
+      type_noise = type_noise
+    )
+  }
+  if(init_type=="udv_si_svd"){
+    suppressWarnings(
+    s <- softImpute(Y, rank.max=K,type="svd", lambda=0)
+    )
+    l <-  s$u[,1:K]%*%diag(s$d[1:K])
+    f <- s$v[,1:K]
+    cEBMF.obj <- list(
+      Y          = Y,
+      Y_fit      = 0*Y,
+      loading    = l , #first moment
+      factor     = f,
+      loading2   = l^2,#second moment
+      factor2    = f^2,
+      tau        = matrix(1, ncol = ncol(Y), nrow=nrow(Y)),
+      X_l        = X_l,
+      X_f        = X_f,
+      K          = K,
+      type_noise = type_noise
+    )
+  }
   class(cEBMF.obj) <- "cEBMF"
   #initialize tau using observed variance
   tau <- update_tau.cEBMF(cEBMF.obj)$tau
   return(cEBMF.obj)
 }
 
-#'@param cEBMF a cEBMF object
-#'@param k the factor to be updated
-#'
-#'
-update_cEBMF <-  function(cEBMF.obj, k)
-{
-
-  #loading update
-  l_k <- cal_expected_loading( cEBMF.obj,k)
-  t_data <- set_data_mococomo(betahat = l_k$l_i_hat,
-                              se      = l_k$s_i,
-                              X       = cEBMF.obj$X_l )
-  t_fit <- fit.mococomo(t_data)
-  fitted_loading <- post_mean_sd.mococomo (t_fit )
-  cEBMF.obj$loading[,k] <-  fitted_loading$mean
-  cEBMF.obj$loading2[,k] <- fitted_loading$sd^2+ fitted_loading$mean^2
-
-
-  #factor update
-
-  f_k <- cal_expected_factor( cEBMF.obj,k)
-  t_data <- set_data_mococomo(betahat = f_k$f_j_hat,
-                              se      = f_k$s_j,
-                              X       = cEBMF.obj$X_f )
-  t_fit <- fit.mococomo(t_data)
-  fitted_factor <- post_mean_sd.mococomo (t_fit )
-  cEBMF.obj$factor[,k] <-  fitted_factor $mean
-  cEBMF.obj$factor2[,k] <-  fitted_factor$sd^2+ fitted_factor$mean^2
-  return(cEBMF.obj)
-}
 
 #'@param cEBMF a cEBMF object
 #'@param k component of interest
 #'@return list of two l_i_hat the estimate loading, s_i the estimated standard errors
-cal_expected_loading <- function( cEBMF.obj,k){
+cal_expected_loading <- function( cEBMF.obj, Rk,k){
 
   f       <- cEBMF.obj$factor[,k]
-  mat_up  <- cEBMF.obj$Y*cEBMF.obj$tau *f
-  mat_low <- cEBMF.obj$tau *(f^2)
+  f2      <- cEBMF.obj$factor2[,k]
+  mat_up  <- sweep(Rk*cEBMF.obj$tau ,2,f, "*")
+  mat_low <- sweep(cEBMF.obj$tau,2,  f2 , "*")
   deno    <- apply(mat_low,1,sum)
 
 
@@ -87,11 +140,12 @@ cal_expected_loading <- function( cEBMF.obj,k){
 #'@param cEBMF a cEBMF object
 #'@param k  component of interest
 #'@return list of two l_i_hat the estimate loading, s_i the estimated standard errors
-cal_expected_factor <- function( cEBMF.obj,k){
+cal_expected_factor <- function( cEBMF.obj, Rk,k){
 
   l       <- cEBMF.obj$loading[,k]
-  mat_up  <- cEBMF.obj$Y*cEBMF.obj$tau *l
-  mat_low <- cEBMF.obj$tau *(l^2)
+  l2      <- cEBMF.obj$loading2[,k]
+  mat_up  <- Rk*cEBMF.obj$tau *l
+  mat_low <- cEBMF.obj$tau *(l2)
   deno    <- apply(mat_low,2,sum)
 
 
@@ -138,6 +192,32 @@ return(R2)
 
 }
 
+
+cal_partial_residuals.cEBMF <- function(cEBMF.obj,k)
+{
+  K <- cEBMF.obj$K
+  if (K > 1){
+
+    id_k <- (1:K)[ - ( (k-1)%%(K+1)) ]
+    partial_pred <-   Reduce("+",
+                                     lapply( id_k,
+                                             function(k)
+                                               (cEBMF.obj$loading[ ,k])%*%t(cEBMF.obj$factor[ ,k])
+                                     )
+
+                               )
+  }else{
+    partial_pred <- 0*cEBMF.obj$Y
+  }
+
+
+  Rk  <-  cEBMF.obj$Y  -   partial_pred
+
+  return(Rk)
+
+}
+
+
 #'@title Estimate noise value
 #'@'description Estimate noise value for different type of structure
 #'@param cEBMF an CEBMF object
@@ -170,5 +250,54 @@ update_tau.cEBMF <- function(cEBMF.obj )
                               byrow=FALSE
                              )
   }
+  return(cEBMF.obj)
+}
+
+#'@param cEBMF a cEBMF object
+
+cEBMF_iter <- function(cEBMF.obj){
+
+  for( k in 1:cEBMF.obj$K){
+
+    cEBMF.obj <- update_cEBMF(cEBMF.obj, k)
+    cEBMF.obj <- update_tau.cEBMF (cEBMF.obj)
+  }
+  return(cEBMF.obj)
+
+}
+
+
+
+#'@param cEBMF a cEBMF object
+#'@param k the factor to be updated
+#'
+#'
+update_cEBMF <-  function(cEBMF.obj, k)
+{
+
+  #loading update
+  Rk <- cal_partial_residuals.cEBMF(cEBMF.obj,k)
+  l_k <- cal_expected_loading( cEBMF.obj, Rk,k)
+  t_data <- set_data_mococomo(betahat = l_k$l_i_hat,
+                              se      = l_k$s_i,
+                              X       = cEBMF.obj$X_l )
+  t_fit <- fit.mococomo(t_data)
+
+  fitted_loading <- post_mean_sd.mococomo (t_fit )
+  cEBMF.obj$loading[,k] <-  fitted_loading$mean
+  cEBMF.obj$loading2[,k] <- fitted_loading$sd^2+ fitted_loading$mean^2
+
+  #factor update
+
+  f_k <- cal_expected_factor( cEBMF.obj,Rk,k)
+  t_data <- set_data_mococomo(betahat = f_k$f_j_hat,
+                              se      = f_k$s_j,
+                              X       = cEBMF.obj$X_f )
+  t_fit <- fit.mococomo(t_data)
+
+  fitted_factor <- post_mean_sd.mococomo (t_fit )
+  cEBMF.obj$factor[,k]  <-  fitted_factor $mean
+  cEBMF.obj$factor2[,k] <-  fitted_factor$sd^2+ fitted_factor$mean^2
+
   return(cEBMF.obj)
 }
--- tests/draft_test_cEBMF.R
+++ tests/draft_test_cEBMF.R
@@ -1,11 +1,11 @@
-library(flashr)
+
 #rm(list = ls())
 library(logisticsusie)  #Simulate data under the mococomo model
 sim1  <- sim_twococomo(n=1000)
 
 
 tt <- fit.mococomo(sim1)
-out <- post_mean_sd.mococomo(fit)
+out <- post_mean_sd.mococomo(tt)
 
 plot( out$mean, sim1$beta)
 #preparing the data
@@ -22,6 +22,8 @@ data2 <-set_data_mococomo(betahat = sim2$betahat,
 
 Y_true <- sim1$beta%*%t(sim2$beta)
 Y <- data1$betahat%*%t(data2$betahat)
+
+plot( Y,Y_true)
 X_l =data1$X
 
 X_f =data2$X
@@ -29,9 +31,27 @@ image(Y)
 image(Y_true)
 
 
-cEBMF.obj <- init_cEBMF (Y, X_l,X_f,K=1)
+
+K=3
+u <- svd(Y,K,K)
+
+str(u)
+u <- softImpute(Y, rank.max=K,type="als", lambda=0)
+plot(u$u%*%diag(u$d[1:K])%*%t(u$v),Y)
+plot(u$u%*%diag(u$d[1:K])%*%t(u$v),Y_true)
 
 
+dim(Y)
+cEBMF.obj <- init_cEBMF (Y, X_l,X_f,K=3, init_type = "udv_si")
+plot(cEBMF.obj$loading[,1],data1$betahat)
+abline(a=0,b=1)
+plot(cEBMF.obj$factor[,1],data2$betahat)
+abline(a=0,b=1)
+
+
+plot(cEBMF.obj$loading%*%t(cEBMF.obj$factor),Y)
+
+plot(cEBMF.obj$loading%*%t(cEBMF.obj$factor),Y_true)
 
 
 for ( o in 1:4){
@@ -40,31 +60,30 @@ for ( o in 1:4){
   t_data <- set_data_mococomo(betahat = l_k$l_i_hat,
                               se      = l_k$s_i,
                               X       = cEBMF.obj$X_l )
-  t_fit <- fit.mococomo(data =t_data)
-  str(t_fit)
+  t_fit <- fit.mococomo(t_data)
 
-  l_i_fit <-  post_mean_sd.mococomo (t_fit )$mean
-  plot(l_i_fit,data1$betahat)
+  fitted_loading <- post_mean_sd.mococomo (t_fit )
+  cEBMF.obj$loading[,k] <-  fitted_loading$mean
+  cEBMF.obj$loading2[,k] <- fitted_loading$sd^2+ fitted_loading$mean^2
 
-
-  cEBMF.obj$loading[,k] <- l_i_fit
-
-
+    #factor update
 
   f_k <- cal_expected_factor( cEBMF.obj,k)
   t_data <- set_data_mococomo(betahat = f_k$f_j_hat,
                               se      = f_k$s_j,
                               X       = cEBMF.obj$X_f )
   t_fit <- fit.mococomo(t_data)
-  f_j_fit <- post_mean_sd.mococomo (t_fit )$mean
-  plot(f_j_fit,data2$betahat)
 
-  cEBMF.obj$loading[,k] <-  post_mean_sd.mococomo (fit )$mean
+  fitted_factor <- post_mean_sd.mococomo (t_fit )
+  cEBMF.obj$factor[,k] <-  fitted_factor $mean
+  cEBMF.obj$factor2[,k] <-  fitted_factor$sd^2+ fitted_factor$mean^2
+
 
   cEBMF.obj<- update_tau.cEBMF (cEBMF.obj )
 
-
-
+  Y_est <- cEBMF.obj$loading[,1]%*%t(cEBMF.obj$factor[,1])
+  plot( Y_est, Y )
+  abline(a=0,b=1)
 }
 
 
@@ -75,85 +94,150 @@ cor( c(Y_true), c(Y_est))
 cor( c(Y_true), c(Y))
 
 
+flash(Y)
 
 
 
 
-#### tesst using ash
+
+####ash ----
 
 library(flashr)
-#rm(list = ls())
-library(logisticsusie)  #Simulate data under the mococomo model
-sim1  <- sim_twococomo(n=1000)
+library(softImpute)
+set.seed(1) # for reproducibility
+ftrue = matrix(rnorm(200), ncol=2)
+ltrue = matrix(rnorm(40), ncol=2)
+ltrue[1:10, 1] = 0 # set up some sparsity
+ltrue[11:20, 2] = 0
+Y = ltrue %*% t(ftrue) + rnorm(2000) # set up a simulated matrix
+f = flash(Y, K=1 ,ebnm_fn= 'ebnm_ash' )
+ldf = f$ldf
+Y_true <- ltrue %*% t(ftrue)
+
+K=3
+u <- softImpute(Y, rank.max=K,type="als", lambda=0)
+plot(u$u%*%diag(u$d[1:K])%*%t(u$v),Y)
+plot(u$u%*%diag(u$d[1:K])%*%t(u$v),Y_true)
 
 
-tt <- fit.mococomo(sim1)
-out <- post_mean_sd.mococomo(fit)
-
-plot( out$mean, sim1$beta)
-#preparing the data
-data1 <-set_data_mococomo(betahat = sim1$betahat,
-                          se = sim1$se ,
-                          X  = sim1$X)
-sim2  <- sim_twococomo(20)
-#preparing the data
-data2 <-set_data_mococomo(betahat = sim2$betahat,
-                          se = sim2$se ,
-                          X  = sim2$X)
+dim(Y)
+X_l <- matrix(rnorm(nrow(Y)*10), nrow= nrow(Y))
+X_f <- matrix(rnorm(ncol(Y)*10), nrow= ncol(Y))
 
 
+cEBMF.obj <- init_cEBMF (Y, X_l,X_f,K=3, init_type = "udv_si")
 
-Y_true <- sim1$beta%*%t(sim2$beta)
-Y <- data1$betahat%*%t(data2$betahat)
-X_l =data1$X
-
-X_f =data2$X
-image(Y)
-image(Y_true)
+abline(a=0,b=1)
 
 
-cEBMF.obj <- init_cEBMF (Y, X_l,X_f,K=1)
+plot(cEBMF.obj$loading%*%t(cEBMF.obj$factor),Y)
+abline(a=0,b=1)
 
+plot(cEBMF.obj$loading%*%t(cEBMF.obj$factor),Y_true)
+abline(a=0,b=1)
 
+t_fit <- lm(c(Y_true)~c(Y))
+summary(t_fit)
 
-
+mean(f$fit$tau)
+mean(cEBMF.obj$tau)
+library(ashr)
 for ( o in 1:4){
   k=1
-  l_k <- cal_expected_loading( cEBMF.obj,k)
+  Rk <- Y
+  l_k <- cal_expected_loading( cEBMF.obj, Rk,k)
+  t_ash <- ash(betahat = l_k$l_i_hat,
+               se      = l_k$s_i,
+               mixcompdist = "normal")
+  cEBMF.obj$loading[,k] <- t_ash$result$PosteriorMean
+  cEBMF.obj$loading2[,k] <- t_ash$result$PosteriorSD^2 + t_ash$result$PosteriorMean^2
+
+  f_k <- cal_expected_factor( cEBMF.obj, Rk,k)
+
+  t_ash <- ash(betahat = f_k$f_j_hat,
+               se      = f_k$s_j,
+               mixcompdist = "normal")
+
+  cEBMF.obj$factor[,k] <- t_ash$result$PosteriorMean
+  cEBMF.obj$factor2[,k] <-  t_ash$result$PosteriorSD^2 + t_ash$result$PosteriorMean^2
+
+
+
+  cEBMF.obj<- update_tau.cEBMF (cEBMF.obj )
+  cEBMF.obj$tau
+  Y_est <- cEBMF.obj$loading[,1]%*%t(cEBMF.obj$factor[,1])
+  plot( Y_est, Y )
+  abline(a=0,b=1)
+}
+
+plot( Y_est, Y_true )
+f = flash(Y, K=1 ,ebnm_fn= 'ebnm_ash' )
+plot( fitted(f),Y_true)
+plot( Y_est, fitted(f) )
+
+
+X_l <- matrix(rnorm(nrow(Y)*10), nrow= nrow(Y))
+X_f <- matrix(rnorm(ncol(Y)*10), nrow= ncol(Y))
+
+
+cEBMF.obj <- init_cEBMF (Y, X_l,X_f,K=3, init_type = "udv_si")
+
+abline(a=0,b=1)
+for ( o in 1:4){
+  k=1
+  Rk <- Y
+  #Rk <- cal_partial_residuals.cEBMF(cEBMF.obj,k)
+  l_k <- cal_expected_loading( cEBMF.obj, Rk,k)
   t_data <- set_data_mococomo(betahat = l_k$l_i_hat,
                               se      = l_k$s_i,
                               X       = cEBMF.obj$X_l )
+  t_fit <- fit.mococomo(t_data)
 
-  t_fit <- ash(t_data$betahat,t_data$se)
-  l_i_fit <-t_fit$result$PosteriorMean
+  fitted_loading <- post_mean_sd.mococomo (t_fit )
+  cEBMF.obj$loading[,k] <-  fitted_loading$mean
+  cEBMF.obj$loading2[,k] <- fitted_loading$sd^2+ fitted_loading$mean^2
 
-  plot(l_i_fit,data1$betahat)
+  #factor update
 
-
-  cEBMF.obj$loading[,k] <- l_i_fit
-
-
-
-  f_k <- cal_expected_factor( cEBMF.obj,k)
+  f_k <- cal_expected_factor( cEBMF.obj,Rk,k)
   t_data <- set_data_mococomo(betahat = f_k$f_j_hat,
                               se      = f_k$s_j,
                               X       = cEBMF.obj$X_f )
-  t_fit <- ash(t_data$betahat,t_data$se)
-  f_j_fit <- t_fit$result$PosteriorMean
-  plot(f_j_fit,data2$betahat)
+  t_fit <- fit.mococomo(t_data)
 
-  cEBMF.obj$loading[,k] <- f_j_fit
+  fitted_factor <- post_mean_sd.mococomo (t_fit )
+  cEBMF.obj$factor[,k] <-  fitted_factor $mean
+  cEBMF.obj$factor2[,k] <-  fitted_factor$sd^2+ fitted_factor$mean^2
+
 
   cEBMF.obj<- update_tau.cEBMF (cEBMF.obj )
 
-
-
+  Y_est <- cEBMF.obj$loading[,1]%*%t(cEBMF.obj$factor[,1])
+  plot( Y_est, Y )
+  abline(a=0,b=1)
 }
+plot( fitted(f),Y_true)
+plot( Y_est, fitted(f) )
 
 
-Y_est <- cEBMF.obj$loading[,1]%*%t(cEBMF.obj$factor[,1])
-plot( Y_est, Y )
-plot( Y_true, Y )
-cor( c(Y_true), c(Y_est))
-cor( c(Y_true), c(Y))
+cEBMF.obj <- init_cEBMF (Y, X_l,X_f,K=3, init_type = "udv_si")
 
+
+
+####Hand run update----
+
+Y_est <- cEBMF.obj$loading[,k]%*%t(cEBMF.obj$factor[,k])
+plot( Y_est, Y_true )
+
+cEBMF.obj <- init_cEBMF (Y, X_l,X_f,K=3, init_type = "udv_si")
+
+for (i in 1:5) {
+  cEBMF.obj <- cEBMF_iter  (cEBMF.obj)
+}
+Y_est <- Reduce("+", lapply( 1:cEBMF.obj$K, function(k) cEBMF.obj$loading[,k]%*%t(cEBMF.obj$factor[,k]) ))
+
+
+plot( Y_est, fitted(f) )
+abline(a=0,b=1)
+plot( Y_est, Y_true)
+points(fitted(f),Y_true  , col="green" )
