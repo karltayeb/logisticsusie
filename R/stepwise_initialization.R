5fe6da8af5841e80a02e70cd0df50756ab6979c5
Karl Tayeb <karl.tayeb7@gmail.com>
2022-10-29 19:00:04 -0500

    plan more initializations

--- R/stepwise_initialization.R
+++ R/stepwise_initialization.R
@@ -1,6 +1,52 @@
 
+# Basic initialzation --------------
+#' check the input data
+.check_X <- function(X) {
+  stopifnot("data$X must be matrix/Matrix" = inherits(X, c("matrix", "Matrix")))
+}
+
+binsusie_prep_data <- function(X, y, N, Z, center = TRUE, scale = FALSE) {
+  # center and scale data
+  .check_X(X) # throws errors if something is wrong
+  X <- Matrix::Matrix(scale(X, center = center, scale = scale))
+
+  # If N was passed as a scalar, convert to vector of length n
+  if (length(N) == 1) {
+    N <- rep(N, length(y))
+  }
+
+  # Make data list
+  # TODO: store X means and standard errors
+  data <- list(X = X, Z = Z, y = y, N = N)
+  return(data)
+}
+
+binsusie_init <- function(X,
+                          y,
+                          N = rep(1, length(y)), # number of trials for each
+                          Z = NULL,
+                          L = min(10, ncol(X)),
+                          scale = FALSE,
+                          center = TRUE,
+                          prior_mean = 0.0, # set prior mean (feature added for Gao and Bohan)
+                          prior_variance = 1.0, # TODO: make scaled prior variance?
+                          prior_weights = NULL) {
+  data <- binsusie_prep_data(X, y, N, Z, scale = scale, center = center)
+  fit <- init.binsusie(
+    data,
+    L = L,
+    prior_mean = prior_mean,
+    prior_variance = prior_variance,
+    prior_weights = prior_weights
+  )
+  return(fit)
+}
+
+
+# Forward initializaton-----------
+
 #' update alpha, mu, and var
-update_b_hard_select.binsusie <- function(fit, fit_intercept = TRUE, fit_prior_variance = TRUE, update_idx = NULL) {
+hard_select_forward <- function(fit, fit_intercept = TRUE, fit_prior_variance = TRUE, update_idx = NULL) {
   if (is.null(update_idx)) {
     update_idx <- seq(fit$hypers$L)
   }
@@ -9,7 +55,7 @@ update_b_hard_select.binsusie <- function(fit, fit_intercept = TRUE, fit_prior_v
     # remove current effect estimate
     shift <- Matrix::drop(shift - compute_Xb.binser(fit, idx = l))
 
-    for (i in 1:10) {
+    for (i in 1:5) {
       # update SER
       post_l <- update_b.binser(fit, idx = l, shift = shift)
       fit$params$mu[l, ] <- post_l$mu
@@ -42,79 +88,18 @@ update_b_hard_select.binsusie <- function(fit, fit_intercept = TRUE, fit_prior_v
 }
 
 
-#' initialize SER
-.forward_init <- function(data, L = 5, init_iter = 10, prior_mean = 0, prior_variance = 1, prior_weights = NULL, kidx = NULL) {
-  fit <- init.binsusie(data, L, prior_mean, prior_variance, prior_weights, kidx)
-
-  # update each single effect
-  # but intervene by setting alpha to a hard selection
-  for (i in 1:init_iter) {
-    fit <- update_b_hard_select.binsusie(fit, fit_intercept = T, fit_prior_variance = F)
-  }
-  return(fit)
-}
-
-
 binsusie_forward_init <- function(X,
                                   y,
                                   N = rep(1, length(y)), # number of trials for each
                                   Z = NULL,
                                   L = min(10, ncol(X)),
+                                  scale = FALSE,
+                                  center = TRUE,
                                   prior_mean = 0.0, # set prior mean (feature added for Gao and Bohan)
                                   prior_variance = 1.0, # TODO: make scaled prior variance?
                                   prior_weights = NULL, # vector of length `p` gjvj g the prior probability of each column of X having nonzero effect...
                                   init_iter = 5) {
-  ##################
-  # checking / setup
-
-  # Make data list
-  data <- list(X = X, Z = Z, y = y, N = N)
-
-  # If N was passed as a scalar, convert to vector of length n
-  if (length(data$N) == 1) {
-    data$N <- rep(N, length(y))
-  }
-  .check_data_binsusie(data) # throws errors if something is wrong
-
-  fit <- .forward_init(
-    data,
-    L = L,
-    init_iter = init_iter,
-    prior_mean = prior_mean,
-    prior_variance = prior_variance,
-    prior_weights = prior_weights
-  )
-  return(fit)
-}
-
-
-binsusie_init <- function(X,
-                          y,
-                          N = rep(1, length(y)), # number of trials for each
-                          Z = NULL,
-                          L = min(10, ncol(X)),
-                          prior_mean = 0.0, # set prior mean (feature added for Gao and Bohan)
-                          prior_variance = 1.0, # TODO: make scaled prior variance?
-                          prior_weights = NULL, # vector of length `p` gjvj g the prior probability of each column of X having nonzero effect...
-                          init_iter = 5) {
-  ##################
-  # checking / setup
-
-  # Make data list
-  data <- list(X = X, Z = Z, y = y, N = N)
-
-  # If N was passed as a scalar, convert to vector of length n
-  if (length(data$N) == 1) {
-    data$N <- rep(N, length(y))
-  }
-  .check_data_binsusie(data) # throws errors if something is wrong
-
-  fit <- init.binsusie(
-    data,
-    L = L,
-    prior_mean = prior_mean,
-    prior_variance = prior_variance,
-    prior_weights = prior_weights
-  )
+  fit <- binsusie_init(X, y, N, Z, L, scale, center, prior_mean, prior_variance, prior_weights)
+  fit <- hard_select_forward(fit, T, F)
   return(fit)
 }
