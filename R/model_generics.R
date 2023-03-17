25d85c710ec671d65866a9d1975180c423541bd3
Karl Tayeb <karl.tayeb7@gmail.com>
2023-03-17 16:11:14 -0400

    S3 generics

--- R/model_generics.R
+++ R/model_generics.R
@@ -3,6 +3,9 @@
 predict_model <- function(x, ...){
   UseMethod("predict_model", x)
 }
+predict_model.default <- function(fit, data){
+  compute_psi(fit, data)
+}
 
 update_model <- function(x, ...){
   UseMethod("update_model", x)
@@ -39,16 +42,20 @@ compute_coef.default <- function(fit){
 compute_Xb <- function(x, ...){
   UseMethod("compute_Xb")
 }
-compute_Xb.default <- function(fit, data){
-  Xb <- drop(data$X %*% colSums(get_alpha(fit) * get_mu(fit)))
-  Zd <- drop(data$Z %*% colSums(get_delta(fit)))
-  return(Matrix::drop(Xb + Zd))
+compute_psi <- function(x, ...){
+  UseMethod("compute_psi")
 }
 
 compute_Xb2 <- function(x, ...){
   UseMethod("compute_Xb2")
 }
+compute_psi2 <- function(x, ...){
+  UseMethod("compute_psi2")
+}
 
+coef <- function(x, ...){
+  UseMethod("coef")
+}
 
 get_alpha <- function(x, ...){
   UseMethod("get_alpha", x)
@@ -58,12 +65,41 @@ get_mu <- function(x, ...){
   UseMethod("get_mu", x)
 }
 
+get_pi <- function(x, ...){
+  UseMethod("get_pi", x)
+}
+
 get_var <- function(x, ...){
   UseMethod("get_var", x)
 }
 
 get_delta <- function(x, ...){
   UseMethod("get_delta", x)
+}
+
+get_mu0 <- function(x, ...){
+  UseMethod("get_mu0", x)
+}
+
+get_var0 <- function(x, ...){
+  UseMethod("get_var0", x)
+}
+
+compute_jj <- function(x, ...){
+  UseMethod("compute_jj", x)
+}
+
+compute_kl <- function(x, ...){
+  UseMethod("compute_kl", x)
+}
+
+compute_elbo <- function(x, ...){
+  UseMethod("compute_elbo", x)
+}
+compute_elbo.default <- function(fit, data) {
+  jj <- sum(compute_jj(fit, data)) # E[p(y | w, b)] - KL[q(b) || p(b)]
+  kl <- compute_kl(fit) # KL[q(b) || p(b)]
+  return(jj - kl)
 }
 
 ### JJ functions
@@ -77,8 +113,8 @@ compute_kappa <- function(data) {
 
 #' update for variational parameter parameter xi q(w) = PG(N, xi)
 update_xi <- function(fit, data) {
-  Xb2 <- compute_Xb2(fit)
-  xi <- sqrt(abs(Xb2))
+  psi2 <- compute_psi2(fit, data)
+  xi <- sqrt(abs(psi2))
   return(xi)
 }
 
@@ -94,4 +130,13 @@ compute_tau <- function(fit, data) {
   omega <- compute_omega(fit, data)
   tau <- Matrix::drop(omega %*% data$X2)
   return(tau)
+}
+
+#' KL(q(b, \omega) || p(\beta, \omega))
+compute_ser_kl <- function(alpha, pi, mu, var, mu0, var0) {
+  kl <- categorical_kl(alpha, pi)
+  kl <- kl + sum(alpha * normal_kl(
+    mu, var, mu0, var0
+  ))
+  return(kl)
 }
