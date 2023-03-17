6c8b3b6caa206cc91a6d6a991fb718eda66d900b
Karl Tayeb <karl.tayeb7@gmail.com>
2023-03-17 11:30:24 -0400

    remove out of scope code

--- tests/testthat/test-prune_model.R
+++
@@ -1,8 +0,0 @@
-test_that("Null model returns 0 PIPs", {
-  set.seed(10)
-  data <- sim_ser(beta = 0, beta0 = -1)
-  fit <- fit.binsusie(data, maxiter = 100, tol = 1e-5)
-  fit_pruned <- prune_model(fit, 1)
-  fit_pruned <- binsusie_wrapup(fit_pruned, 1e-9)
-  expect_equal(fit_pruned$pip, rep(0, length(fit_pruned$pip)))
-})
