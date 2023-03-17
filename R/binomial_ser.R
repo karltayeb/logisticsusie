bc5c5c31f527fa5193173357304c46d4251a0667
Karl Tayeb <karl.tayeb7@gmail.com>
2023-03-17 16:10:40 -0400

    binomial ser supports "random effect" offset

--- R/binomial_ser.R
+++ R/binomial_ser.R
@@ -7,119 +7,22 @@
 # L-indexed getters
 ###
 
-#' idx is for indexing when we run susie
-.get_alpha <- function(fit, idx = NULL) {
-  if (is.null(idx)) {
-    alpha <- fit$params$alpha
-  } else {
-    alpha <- fit$params$alpha[idx, ]
-  }
-  return(alpha)
-}
-
-.get_mu <- function(fit, idx = NULL) {
-  if (is.null(idx)) {
-    mu <- fit$params$mu
-  } else {
-    mu <- fit$params$mu[idx, ]
-  }
-  return(mu)
-}
-
-.get_var <- function(fit, idx = NULL) {
-  if (is.null(idx)) {
-    var <- fit$params$var
-  } else {
-    var <- fit$params$var[idx, ]
-  }
-  return(var)
-}
-
-.get_pi <- function(fit, idx = NULL) {
-  if (is.null(idx)) {
-    pi <- fit$hypers$pi
-  } else {
-    pi <- fit$hypers$pi[idx, ]
-  }
-  return(pi)
-}
-
-.get_delta <- function(fit, idx = NULL) {
-  if (is.null(idx)) {
-    delta <- fit$params$delta
-  } else {
-    delta <- fit$params$delta[idx, ]
-  }
-  return(delta)
-}
-
-.get_prior_mean <- function(fit, idx = NULL) {
-  if (is.null(idx)) {
-    prior_mean <- fit$hypers$prior_mean
-  } else {
-    prior_mean <- fit$hypers$prior_mean[idx]
-  }
-  return(prior_mean)
-}
-
-.get_var0 <- function(fit, idx = NULL) {
-  if (is.null(idx)) {
-    var0 <- fit$hypers$prior_variance
-  } else {
-    var0 <- fit$hypers$prior_variance[idx]
-  }
-  return(var0)
-}
-
-###
-# K-indexed getters
-###
-
-.get_y <- function(fit, kidx = NULL) {
-  if (is.null(fit$kidx)) {
-    y <- fit$data$y
-  } else {
-    y <- fit$data$y[, fit$kidx]
-  }
-  return(y)
-}
-
-.get_N <- function(fit, kidx = NULL) {
-  if (is.null(fit$kidx)) {
-    N <- fit$data$N
-  } else {
-    N <- fit$data$N[, fit$kidx]
-  }
-  return(N)
-}
-
-.get_xi <- function(fit, kidx = NULL) {
-  # if(is.null(fit$kidx)){
-  #   xi <- fit$params$xi
-  # } else{
-  #   xi <- fit$params$xi[, fit$kidx]
-  # }
-  xi <- fit$params$xi
-  return(xi)
-}
-
+get_alpha.binser <- function(fit){fit$alpha}
+get_pi.binser <- function(fit){fit$pi}
+get_mu.binser <- function(fit){fit$mu}
+get_var.binser <- function(fit){fit$var}
+get_delta.binser <- function(fit){fit$delta}
+get_xi.binser <- function(fit){fit$xi}
+get_mu0.binser <- function(fit){fit$mu0}
+get_var0.binser <- function(fit){fit$var0}
 
 #' Get Binomial SER coefficients
 #' Extract regression coefficients from binomial SER fit
 #' @param fit Binomial SER object
 #' @return Return E[\beta]
-coef.binser <- function(fit, idx = NULL) {
-  b <- Matrix::drop(.get_alpha(fit, idx) * .get_mu(fit, idx))
+coef.binser <- function(fit) {
+  b <- Matrix::drop(fit$alpha * fit$mu)
   return(b)
-}
-
-#' KL(q(b, \omega) || p(\beta, \omega))
-.compute_ser_kl <- function(alpha, pi, mu, var, prior_mean, var0) {
-  kl <- categorical_kl(alpha, pi)
-  kl <- kl + sum(alpha * normal_kl(
-    mu, var, prior_mean, var0
-  ))
-  return(kl)
 }
 
 #' compute_kl.binser
@@ -127,19 +30,19 @@ coef.binser <- function(fit, idx = NULL) {
 #' @param fit Binomial SER object
 #' @return Return KL(q(\beta) || p(\beta))
 compute_kl.binser <- function(fit) {
-  return(.compute_ser_kl(
-    .get_alpha(fit),
-    .get_pi(fit),
-    .get_mu(fit),
-    .get_var(fit),
-    .get_prior_mean(fit),
-    .get_var0(fit)
+  return(compute_ser_kl(
+    fit$alpha,
+    fit$pi,
+    fit$mu,
+    fit$var,
+    fit$mu0,
+    fit$var0
   ))
 }
 
 #' fixed effects, intercept, shift
-compute_Zd.binser <- function(fit, idx = NULL, shift = 0) {
-  Zd <- Matrix::drop(fit$data$Z %*% .get_delta(fit, idx)) + shift
+compute_Zd.binser <- function(fit, data) {
+  Zd <- Matrix::drop(data$Z %*% fit$delta)
   return(Zd)
 }
 
@@ -147,58 +50,60 @@ compute_Zd.binser <- function(fit, idx = NULL, shift = 0) {
 #' Compute expected predicted log odds
 #' @param fit Binomial SER object
 #' @return Return E[Xb]
-compute_Xb.binser <- function(fit, idx = NULL, shift = 0) {
-  Xb <- Matrix::drop(fit$data$X %*% coef.binser(fit, idx))
-  Zd <- compute_Zd.binser(fit, idx, shift)
-  Xb <- Xb + Zd
+compute_Xb.binser <- function(fit, data) {
+  Xb <- Matrix::drop(data$X %*% coef(fit))
   return(Xb)
+}
+
+compute_psi.binser <- function(fit, data){
+  Xb <- compute_Xb(fit, data)
+  Zd <- compute_Zd.binser(fit, data)
+  psi <- Xb + Zd + data$shift
+  return(psi)
 }
 
 #' compute_Xb2.binser
 #' Compute second moments of predicted log odds
 #' @param fit Binomial SER object
 #' @return Return E[Xb]
-compute_Xb2.binser <- function(fit, idx = NULL, shift = 0) {
-  Xb <- Matrix::drop(fit$data$X %*% coef.binser(fit, idx))
-  Zd <- compute_Zd.binser(fit, idx, shift)
-
-  b2 <- .get_alpha(fit, idx) * (.get_mu(fit, idx)^2 + .get_var(fit, idx))
-  Xb2 <- Matrix::drop(fit$data$X2 %*% b2)
-
-  Xb2 <- Xb2 + 2 * Xb * Zd + Zd^2
+compute_Xb2.binser <- function(fit, data) {
+  b2 <- fit$alpha * (fit$mu^2 + fit$var)
+  Xb2 <- Matrix::drop(data$X2 %*% b2)
   return(Xb2)
+}
+
+compute_psi2.binser <- function(fit, data){
+  Xb2 <- compute_Xb2(fit, data)
+  Xb <- compute_Xb(fit, data)
+  VXb <- Xb2 - Xb^2
+  shift <- data$shift
+  Zd <- compute_Zd.binser(fit, data)
+  psi2 <- (Xb + Zd + data$shift)^2 + VXb + data$shift_var
+  return(psi2)
 }
 
 #' Compute a scaled posterior mean
 #' TODO: add support for non-zero prior mean
 #' @param fit a SER object
-compute_nu.binser <- function(fit, idx = NULL, kidx = NULL, shift = 0) {
-  prior_mean <- .get_prior_mean(fit, idx = idx)
-  tau0 <- 1. / .get_var0(fit, idx = idx)
+compute_nu.binser <- function(fit, data) {
+  omega <- compute_omega(fit, data)
+  kappa <- compute_kappa(data)
 
-  kappa <- compute_kappa(fit, kidx)
-  Zd <- compute_Zd.binser(fit, idx, shift)
-  omega <- compute_omega(fit)
-  tmp <- kappa - omega * Zd
-  nu <- Matrix::drop(tmp %*% fit$data$X) + (prior_mean * tau0)
+  tau0 <- 1. / fit$var0
+  prior_mean <- fit$mu0
+
+  Zd <- compute_Zd.binser(fit, data)
+  tmp <- kappa - omega * (Zd + data$shift)
+  nu <- Matrix::drop(tmp %*% data$X) + (prior_mean * tau0)
   return(nu)
 }
 
-
-
-#' update for variational parameter parameter xi q(w) = PG(N, xi)
-update_xi.binser <- function(fit, shift = 0) {
-  Xb2 <- compute_Xb2.binser(fit, shift = shift)
-  xi <- sqrt(abs(Xb2))
-  return(xi)
-}
-
 #' update intercept and fixed effect covariates
-update_delta.binser <- function(fit, idx = NULL, kidx = NULL, shift = 0) {
-  Z <- fit$data$Z
-  omega <- compute_omega(fit)
-  Xb <- fit$data$X %*% coef.binser(fit, idx) + shift
-  kappa <- compute_kappa(fit, kidx)
+update_delta.binser <- function(fit, data) {
+  omega <- compute_omega(fit, data)
+  kappa <- compute_kappa(data)
+  Xb <- compute_Xb(fit, data) + data$shift
+  Z <- data$Z
   delta <- Matrix::drop(Matrix::solve((omega * t(Z)) %*% Z, t(Z) %*% (kappa - omega * Xb)))
   return(delta)
 }
@@ -207,7 +112,7 @@ update_delta.binser <- function(fit, idx = NULL, kidx = NULL, shift = 0) {
 #' @param nu
 #' @param tau
 #' @param pi prior probability vector (length p)
-.update_b_ser <- function(nu, tau, pi) {
+update_b_ser <- function(nu, tau, pi) {
   logits <- log(pi) - 0.5 * log(tau) + 0.5 * nu^2 / tau
   logits <- logits - logSumExp(logits)
   alpha <- exp(logits)
@@ -221,170 +126,106 @@ update_delta.binser <- function(fit, idx = NULL, kidx = NULL, shift = 0) {
 }
 
 #' update q(b)
-update_b.binser <- function(fit, idx = NULL, kidx = NULL, shift = 0) {
-  nu <- Matrix::drop(compute_nu.binser(fit, idx, kidx, shift))
-  tau0 <- 1 / .get_var0(fit, idx)
-  tau <- tau0 + fit$params$tau
-  post <- .update_b_ser(nu, tau, .get_pi(fit, idx))
+update_b.binser <- function(fit, data) {
+  nu <- Matrix::drop(compute_nu.binser(fit, data))
+  tau0 <- 1 / fit$var0
+  tau <- tau0 + fit$tau
+  post <- update_b_ser(nu, tau, fit$pi)
   return(post)
 }
 
-
 #' Variational M step for updating prior_variance
 #' Optimize ELBO wrt to prior effect standard deviation
-update_prior_variance.binser <- function(fit, idx = NULL) {
-  b2 <- .get_alpha(fit, idx) * (.get_mu(fit, idx)^2 + .get_var(fit, idx))
-  b <- coef.binser(fit, idx)
-  prior_mean <- .get_prior_mean(fit, idx)
-  prior_variance <- sum(b2 - 2 * b * prior_mean) + prior_mean^2
-  return(prior_variance)
+update_var0.binser <- function(fit) {
+  b2 <- fit$alpha * (fit$mu^2 + fit$var)
+  b <- coef(fit)
+  mu0 <- fit$mu0
+  var0 <- sum(b2 - 2 * b * mu0) + mu0^2
+  return(var0)
 }
 
+
+update_model.binser <- function(fit, data,
+                                fit_alpha = TRUE,
+                                fit_intercept=TRUE,
+                                fit_prior_variance=TRUE,
+                                fit_xi = TRUE,
+                                track_elbo = TRUE){
+  # update b
+  post <- update_b.binser(fit, data)
+  fit$mu <- post$mu
+  fit$var <- post$var
+
+  if(fit_alpha){
+    fit$alpha <- post$alpha
+  }
+
+  # update intercept/fixed effect covariates
+  if (fit_intercept) {
+    fit$delta <- update_delta.binser(fit, data)
+  }
+  if (fit_prior_variance) {
+    fit$var0 <- update_var0.binser(fit)
+  }
+  # update xi
+  if (fit_xi){
+    fit$xi <- update_xi(fit, data)
+    fit$tau <- compute_tau(fit, data)
+  }
+
+  if(track_elbo){
+    fit$elbo <- c(fit$elbo, compute_elbo(fit, data))
+  }
+
+  return(fit)
+}
 
 #' Compute E[p(z, w| b) - q(w)], which is the same as
 #' the bound on the logistic function proposed by Jaakkola and Jordan
 #' NOTE: this only works when xi is updated!
 #' @returns a vector of length N with the lower bound for each data point
-jj_bound.binser <- function(fit, shift = 0, kidx = NULL) {
-  xi <- .get_xi(fit, kidx = kidx)
-  Xb <- compute_Xb.binser(fit, shift = shift)
-  kappa <- compute_kappa(fit, kidx = kidx)
-  n <- .get_N(fit)
-
-  # Xb2 <- compute_Xb2.binser(fit)
-  # omega <- compute_omega(fit)
-
-  bound <- n * log(sigmoid(xi)) + (kappa * Xb) - (0.5 * n * xi) #+ 0.5 * omega * (xi^2 - Xb2)
+compute_jj.binser <- function(fit, data) {
+  xi <- fit$xi
+  psi <- compute_psi(fit, data)
+  kappa <- compute_kappa(data)
+  n <- data$N
+  bound <- n * log(sigmoid(xi)) + (kappa * psi) - (0.5 * n * xi)
   return(bound)
 }
 
-explicit_elbo.binser <- function(fit) {
-  Xb <- compute_Xb.binser(fit)
-  kappa <- compute_kappa(fit)
-  xi <- .get_xi(fit)
-  n <- .get_N(fit)
-
-  Xb2 <- compute_Xb2.binser(fit)
-  omega <- compute_omega(fit)
-
-  ll <- -n * log(2) + kappa * Xb - 0.5 * Xb2 * omega
-  kl <- pg_kl(n, xi)
-
-  return(ll - kl)
-}
-
-#' More explicit, only works for Bernoulli observations
-jj_bound.logistic <- function(fit, shift = 0, kidx = NULL) {
-  xi <- .get_xi(fit, kidx)
-  Xb <- compute_Xb.binser(fit, shift = shift)
-  kappa <- compute_kappa(fit, kidx)
-
-  Xb2 <- compute_Xb2.binser(fit, shift = shift)
-  omega <- compute_omega(fit)
-  bound <- kappa * Xb - 0.5 * xi - 0.25 / xi * tanh(xi / 2) * (Xb2 - xi^2) - log(1 + exp(-xi))
-  return(bound)
-}
-
-
-compute_elbo.binser <- function(fit, shift = 0) {
-  jj <- sum(jj_bound.binser(fit, shift = shift)) # E[p(y | w, b)] - KL[q(b) || p(b)]
-  kl <- compute_kl.binser(fit) # KL[q(b) || p(b)]
+compute_elbo.binser <- function(fit, data) {
+  jj <- sum(compute_jj(fit, data)) # E[p(y | w, b)] - KL[q(b) || p(b)]
+  kl <- compute_kl(fit) # KL[q(b) || p(b)]
   return(jj - kl)
 }
 
 
-#' initialize SER
-#' @param data a list containing X, Z, y
-#' @param prior_mean prior effect mean
-#' @param simga0 prior effect standard deviation
-init.binser <- function(data, prior_mean = 0, prior_variance = 1, prior_weights = NULL) {
-  n <- nrow(data$X)
-  p <- ncol(data$X) # number of covariates to select
-  p2 <- ncol(data$Z) # number of fixed effects + intercept
-
+# Initialize -------
+initialize_binser <- function(n, p, p2, mu0=0, var0=1, pi=rep(1/p, p)) {
   # initialize variational parameters
-  params <- list(
-    alpha = rep(1, p) / p,
-    mu = rep(0, p),
-    var = rep(1, p),
+  ser <- list(
+    alpha = pi,  # initialize posterior to prior
+    mu = rep(mu0, p),
+    var = rep(var0, p),
     delta = rep(0, p2),
     xi = rep(1e-3, n),
-    tau = 0 # initialized at end of this function
+    pi = pi,
+    mu0 = mu0,
+    var0 = var0,
+    tau = rep(1, p), # initialized at end of this function
+    elbo = -Inf
   )
-
-  # initialize hyper-parameters
-  if (is.null(prior_weights)) {
-    prior_weights <- rep(1, p) / p
-  }
-  hypers <- list(
-    pi = prior_weights, # categorical probability of nonzero effect
-    prior_mean = prior_mean, # prior mean of effect
-    prior_variance = prior_variance # prior variance of effect, TODO: include as init arg
-  )
-
-  data$X2 <- data$X^2
-
-  # TODO: check that data has y, N, X, Z
-  fit.init <- list(
-    data = data,
-    params = params,
-    hypers = hypers,
-    elbo = c(-Inf)
-  )
-  fit.init$params$tau <- compute_tau(fit.init)
-  return(fit.init)
+  class(ser) <- 'binser'
+  return(ser)
 }
 
-iter.binser <- function(fit, shift = 0, fit_intercept = TRUE, fit_prior_variance = TRUE) {
-  # update b
-  post <- update_b.binser(fit, shift = shift)
-  fit$params$mu <- post$mu
-  fit$params$var <- post$var
-  fit$params$alpha <- post$alpha
-
-  # update intercept/fixed effect covariates
-  if (fit_intercept) {
-    fit$params$delta <- update_delta.binser(fit, shift = shift)
-  }
-  if (fit_prior_variance) {
-    fit$hypers$prior_variance <- update_prior_variance.binser(fit)
-  }
-
-  # update xi
-  fit$params$xi <- update_xi.binser(fit, shift = shift)
-  fit$params$tau <- compute_tau(fit)
-
-  return(fit)
-}
-
-#' Fit the binomial single effect regression
-fit.binser <- function(data = NULL, fit = NULL,
-                       maxiter = 10,
-                       tol = 1e-3,
-                       fit_intercept = TRUE,
-                       fit_prior_variance = TRUE,
-                       shift = NULL) {
-  # no shift
-  if (is.null(shift)) {
-    shift <- 0
-  }
-
-  # initialize model if no provided
-  if (is.null(fit)) {
-    fit <- init.binser(data)
-  }
-
-  # iteration loop
-  for (i in 1:maxiter) {
-    fit <- iter.binser(fit)
-
-    # compute elbo -- TODO: do we want to record every time?
-    fit$elbo <- c(fit$elbo, compute_elbo.binser(fit))
-    if (.converged(fit, tol)) {
-      break
-    }
-  }
-  return(fit)
+data_initialize_binser <- function(data, mu0=0, var0=1){
+  n <- nrow(data$X)
+  p <- ncol(data$X)
+  p2 <- ncol(data$Z)
+  ser <- initialize_binser(n, p, p2, mu0, var0)
+  ser$tau <- compute_tau(ser, data)
+  return(ser)
 }
 
 
@@ -426,26 +267,38 @@ binser <- function(X,
                    tol = 0.001,
                    verbose = FALSE,
                    n_purity = 100) {
-  # Initialize model
+
+  # Prepare data
   data <- binsusie_prep_data(X, y, N, Z, scale = scale, center = center)
-  if (is.null(s_init)) {
-    fit <- init.binser(data, prior_mean = prior_mean, prior_variance = prior_variance, prior_weights = prior_weights)
+
+  # Initialize model
+  fit <- data_initialize_binser(data, prior_mean, prior_variance)
+
+  # Fit model
+  fit <- fit_model(fit, data,
+                   fit_intercept=intercept,
+                   fit_prior_variance = estimate_prior_variance,
+                   track_elbo = TRUE)
+  return(fit)
+}
+
+
+print.binser <- function(fit){
+  cs <- get_cs(fit$alpha)
+  if(cs$size < 10){
+    cs_msg <- paste0('CS = {', paste(cs$cs, collapse=', '), '}')
   } else {
-    fit <- s_init
+    cs_msg <- paste0('CS = {', paste(head(cs$cs, 10), collapse=', '), ', ... }')
   }
 
-  # model fitting
-  for (i in 1:max_iter) {
-    fit <- iter.binser(
-      fit,
-      shift = o,
-      fit_intercept = intercept,
-      fit_prior_variance = estimate_prior_variance
-    )
-    fit$elbo <- c(fit$elbo, compute_elbo.binser(fit, shift = o))
-    if (.converged(fit, tol)) {
-      break
-    }
+  if(is.null(fit$elbo)){
+    fit$elbo <- -Inf
   }
-  return(fit)
+  message <- paste0(
+    'Binomial SER:',
+    '\n ELBO = ', tail(fit$elbo, 1),
+    '\n prior_variance = ', fit$var0,
+    '\n ', cs_msg
+  )
+  cat(message)
 }
