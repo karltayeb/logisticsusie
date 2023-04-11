a7b2d4db752a5d34c8be4f2994441d95ea8d078b
Karl Tayeb <karl.tayeb7@gmail.com>
2023-04-11 12:00:32 -0400

    support variance in offset

--- R/univariate_vb.R
+++ R/univariate_vb.R
@@ -1,12 +1,12 @@
 # Univariate VB Updates --------
-update_intercept_vb <- function(x, y, o, mu, tau, xi, delta, tau0) {
+update_intercept_vb <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
   kappa <- y - 0.5
   xb <- (x * mu) + o
   omega <- pg_mean(1, xi)
   return(sum(kappa - xb * omega) / sum(omega))
 }
 
-update_b_vb <- function(x, y, o, mu, tau, xi, delta, tau0) {
+update_b_vb <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
   delta <- delta + o
   omega <- pg_mean(1, xi)
   kappa <- y - 0.5
@@ -15,61 +15,62 @@ update_b_vb <- function(x, y, o, mu, tau, xi, delta, tau0) {
   return(list(mu = nu / tau, tau = tau))
 }
 
-update_xi_vb <- function(x, y, o, mu, tau, xi, delta, tau0) {
-  delta <- delta + o
-  xb2 <- x^2 * (mu^2 + 1 / tau) + 2 * x * mu * delta + delta^2
-  return(sqrt(xb2))
+compute_psi_vb <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
+  psi <- x * mu + delta + o
+  return(psi)
 }
 
-compute_elbo_vb <- function(x, y, o, mu, tau, xi, delta, tau0) {
+compute_psi2_vb <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
+  Vo <- o2 - o^2
+  Vxb <- x^2 / tau
+
+  xb <- x * mu
+  psi2 <- (xb + delta + o)^2 + Vo + Vxb
+
+  if(any(is.nan(sqrt(psi2)))){browser()}
+  return(psi2)
+}
+
+update_xi_vb <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
+  psi2 <- compute_psi2_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
+  return(sqrt(psi2))
+}
+
+compute_elbo_vb <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
   kappa <- y - 0.5
-  delta <- delta + o
-  xb <- (x * mu) + delta
-  bound <- log(sigmoid(xi)) + (kappa * xb) - (0.5 * xi)
+  psi <- compute_psi_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
+  bound <- log(sigmoid(xi)) + (kappa * psi) - (0.5 * xi)
   kl <- normal_kl(mu, 1 / tau, 0, 1 / tau0)
   return(sum(bound) - kl)
 }
 
 # include the term that cancels out when xi is up-to-date
-compute_elbo_vb2 <- function(x, y, o, mu, tau, xi, delta, tau0) {
+compute_elbo_vb2 <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
   kappa <- y - 0.5
   delta <- delta + o
-  xb <- (x * mu) + delta
-  xb2 <- x^2 * (mu^2 + 1 / tau) + 2 * x * mu * delta + delta^2
+  psi <- compute_psi_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
+  psi2 <- compute_psi2_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
+
   omega <- pg_mean(1, xi)
-  bound <- log(sigmoid(xi)) + (kappa * xb) - (0.5 * xi) + 0.5 * omega * (xi^2 - xb2)
+  bound <- log(sigmoid(xi)) + (kappa * psi) - (0.5 * xi) + 0.5 * omega * (xi^2 - psi2)
   kl <- normal_kl(mu, 1 / tau, 0, 1 / tau0)
   return(sum(bound) - kl)
 }
 
 # update xi on the fly, so bound is tight
-compute_elbo_vb3 <- function(x, y, o, mu, tau, xi, delta, tau0) {
+compute_elbo_vb3 <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
   kappa <- y - 0.5
-  delta <- delta + o
-  xb <- (x * mu) + delta
-  xb2 <- x^2 * (mu^2 + 1 / tau) + 2 * x * mu * delta + delta^2
-  xi <- sqrt(xb2)
+  psi <- compute_psi_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
+  psi2 <- compute_psi2_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
+  xi <- sqrt(psi2)
   omega <- pg_mean(1, xi)
-  bound <- log(sigmoid(xi)) + (kappa * xb) - (0.5 * xi)
-  kl <- normal_kl(mu, 1 / tau, 0, 1 / tau0)
-  return(sum(bound) - kl)
-}
-
-# deal with offsets so you can compute BFs for each individual SER?
-compute_elbo_vb4 <- function(x, y, o, mu, tau, xi, delta, tau0, offset, offset2) {
-  kappa <- y - 0.5
-  delta <- delta + o
-  xb <- (x * mu) + delta
-  xb2 <- x^2 * (mu^2 + 1 / tau) + 2 * x * mu * delta + delta^2
-  xi <- sqrt(xb2)
-  omega <- pg_mean(1, xi)
-  bound <- log(sigmoid(xi)) + (kappa * xb) - (0.5 * xi)
+  bound <- log(sigmoid(xi)) + (kappa * psi) - (0.5 * xi)
   kl <- normal_kl(mu, 1 / tau, 0, 1 / tau0)
   return(sum(bound) - kl)
 }
 
 #' Update prior precision of effect
-update_tau0_vb <- function(x, y, o, mu, tau, xi, delta, tau0) {
+update_tau0_vb <- function(x, y, o, o2, mu, tau, xi, delta, tau0) {
   b2 <- (mu^2 + 1 / tau)
   return(1 / b2)
 }
@@ -79,7 +80,7 @@ update_tau0_vb <- function(x, y, o, mu, tau, xi, delta, tau0) {
 #' The intercept `delta` is treated as a parameter to be optimized,
 #' Normal prior on the effect N(0, 1/tau0)
 #' @export
-fit_univariate_vb <- function(x, y, o = 0,
+fit_univariate_vb <- function(x, y, o = 0, o2 = 0,
                               delta.init = logodds(mean(y) + 1e-10),
                               tau0 = 1,
                               estimate_intercept = T,
@@ -89,22 +90,22 @@ fit_univariate_vb <- function(x, y, o = 0,
   mu <- 0
   tau <- 1
   delta <- delta.init
-  xi <- update_xi_vb(x, y, o, mu, tau, 1, delta, tau0)
+  xi <- update_xi_vb(x, y, o, o2, mu, tau, 1, delta, tau0)
   xi <- pmax(xi, 1e-3)
 
-  elbos <- compute_elbo_vb(x, y, o, mu, tau, xi, delta, tau0)
+  elbos <- compute_elbo_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
   for (i in seq(maxit)) {
     # rep
     if (estimate_intercept) {
-      delta <- update_intercept_vb(x, y, o, mu, tau, xi, delta, tau0)
+      delta <- update_intercept_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
     }
 
-    b <- update_b_vb(x, y, o, mu, tau, xi, delta, tau0)
+    b <- update_b_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
     mu <- b$mu
     tau <- b$tau
 
-    xi <- update_xi_vb(x, y, o, mu, tau, xi, delta, tau0)
-    elbos <- c(elbos, compute_elbo_vb(x, y, o, mu, tau, xi, delta, tau0))
+    xi <- update_xi_vb(x, y, o, o2, mu, tau, xi, delta, tau0)
+    elbos <- c(elbos, compute_elbo_vb(x, y, o, o2, mu, tau, xi, delta, tau0))
 
     if (diff(tail(elbos, 2)) < tol) {
       break
@@ -114,7 +115,7 @@ fit_univariate_vb <- function(x, y, o = 0,
   converged <- diff(tail(elbos, 2)) < tol
   monotone <- .monotone(elbos)
   return(list(
-    x = x, y = y, o = o,
+    x = x, y = y, o = o, o2=o2,
     mu = mu, tau = tau, xi = xi, delta = delta, tau0 = tau0,
     elbos = elbos,
     converged = converged,
@@ -125,7 +126,7 @@ fit_univariate_vb <- function(x, y, o = 0,
 # SER--------
 #' Fit a logistic single effect regression model using univariate VB approximation
 #' @export
-fit_uvb_ser <- function(X, y, o = NULL,
+fit_uvb_ser <- function(X, y, o = NULL, o2=NULL,
                         prior_variance = 1.0,
                         intercept.init = logodds(mean(y) + 1e-10),
                         estimate_intercept = T,
@@ -136,13 +137,16 @@ fit_uvb_ser <- function(X, y, o = NULL,
     # fixed offsets
     o <- 0
   }
+  if (is.null(o2)) {
+    o2 <- o^2
+  }
 
-  # null_model_elbo <- tail(fit_univariate_vb(X[, 1], y, o = o, tau0 = 1e10)$elbos, 1)
-  null_likelihood <- sum(dbinom(y, 1, mean(y), log = T))
+  #null_likelihood <- sum(dbinom(y, 1, mean(y), log = T))
+  null_likelihood <- tail(fit_univariate_vb(X[, 1], y, o = o, o2=o2, tau0 = 1e10)$elbos, 1)
 
   res <- purrr::map(1:p, ~ fit_univariate_vb(
     X[, .x], y,
-    o = o,
+    o = o, o2 = o2,
     tau0 = tau0,
     delta.init = intercept.init,
     estimate_intercept = estimate_intercept
