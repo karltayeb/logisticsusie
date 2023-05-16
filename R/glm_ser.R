d1a33cb2e3a94e823a8739ab1252971b4d2674a8
Karl Tayeb <karl.tayeb7@gmail.com>
2023-05-16 11:08:54 -0500

    add ser function for fitting glm with corrected approximate bayes factors

--- R/glm_ser.R
+++ R/glm_ser.R
@@ -2,7 +2,9 @@
 
 # compute SER using GLM
 #' @export
-fit_glm_ser <- function(X, y, o = NULL, prior_variance = 1.0, estimate_intercept = T, prior_weights = NULL, family = "binomial") {
+fit_glm_ser <- function(X, y, o = NULL,
+                        prior_variance = 1.0, estimate_intercept = T,
+                        prior_weights = NULL, family = "binomial") {
   p <- ncol(X)
   betahat <- numeric(p)
   shat2 <- numeric(p)
@@ -63,6 +65,83 @@ fit_glm_ser <- function(X, y, o = NULL, prior_variance = 1.0, estimate_intercept
     mu = post_mean,
     var = post_var,
     lbf = lbf,
+    lbf_model = lbf_model,
+    prior_variance = prior_variance,
+    loglik = loglik,
+    betahat = betahat,
+    shat2 = shat2,
+    intercept = intercept
+  )
+  return(res)
+}
+
+#' compute SER using GLM, use Laplace approximation to BF rather than ABF
+#' @export
+fit_glm_ser2 <- function(X, y, o = NULL,
+                         prior_variance = 1, estimate_intercept = T,
+                         prior_weights = NULL, family = "binomial") {
+  p <- ncol(X)
+  betahat <- numeric(p)
+  shat2 <- numeric(p)
+  intercept <- rep(0, p)
+  lr <- rep(0, p)
+
+  if (is.null(o)) {
+    o <- rep(0, length(y))
+  }
+
+  for (j in 1:p) {
+    if (estimate_intercept) {
+      log.fit <- glm(y ~ X[, j] + 1 + offset(o), family = family)
+      intercept[j] <- unname(coef(log.fit)[1])
+    }
+    else {
+      log.fit <- glm(y ~ X[, j] - 1 + offset(o), family = family)
+    }
+    log.fit.coef <- summary(log.fit)$coefficients
+    betahat[j] <- ifelse(is.na(log.fit.coef[1 + estimate_intercept,
+                                            1]), 0, log.fit.coef[1 + estimate_intercept, 1])
+    shat2[j] <- ifelse(is.na(log.fit.coef[1 + estimate_intercept,
+                                          2]), Inf, log.fit.coef[1 + estimate_intercept, 2]^2)
+    lr[j] <- 0.5 * (log.fit$null.deviance - log.fit$deviance)
+  }
+
+  # compute ABF
+  lbf <- dnorm(betahat, 0, sqrt(prior_variance + shat2), log = TRUE) -
+    dnorm(betahat, 0, sqrt(shat2), log = TRUE)
+
+  # compute liklihood ratio under asymptotic approximation
+  alr_mle <- dnorm(betahat, betahat, sqrt(shat2), log=T) -
+    dnorm(betahat, 0, sqrt(shat2), log=T)
+
+  # corrected ABF
+  lbf <- lbf - alr_mle + lr
+
+  lbf[is.infinite(shat2)] <- 0
+  if (is.null(prior_weights)) {
+    prior_weights <- rep(1/p, p)
+  }
+  maxlbf <- max(lbf)
+  w <- exp(lbf - maxlbf)
+  w_weighted <- w * prior_weights
+  weighted_sum_w <- sum(w_weighted)
+  alpha <- w_weighted/weighted_sum_w
+
+  # posterior
+  post_var <- 1/((1/shat2) + (1/prior_variance))
+  post_mean <- (1/shat2) * post_var * betahat
+  post_mean2 <- post_var + post_mean^2
+  lbf_model <- maxlbf + log(weighted_sum_w)
+  null_loglik <- sum(dbinom(y, 1, mean(y), log = T))
+  loglik <- lbf_model + null_loglik
+
+  res <- list(
+    alpha = alpha,
+    mu = post_mean,
+    var = post_var,
+    lbf = lbf,
+    lr = lr,
+    null_loglik = null_loglik,
     lbf_model = lbf_model,
     prior_variance = prior_variance,
     loglik = loglik,
---
+++ tests/testthat/test-ibss_from_ser.R
@@ -0,0 +1,16 @@
+test_that("Compare ABF vs corrected ABF in SuSiE IBSS", {
+  sim <- logisticsusie::sim_susie()
+  fit1 <- with(sim, ibss_from_ser(X, y, 5, prior_variance = 1, ser_function = fit_glm_ser))
+  fit2 <- with(sim, ibss_from_ser(X, y, 5, prior_variance = 1, ser_function = fit_glm_ser2))
+
+  pip1 <- susieR::susie_get_pip(fit1$alpha)
+  pip2 <- susieR::susie_get_pip(fit2$alpha)
+  fit1$fits[[1]]$lbf - fit2$fits[[1]]$lbf
+})
+
+
+test_that("Compare ABF vs correct ABF in SER", {
+  sim <- logisticsusie::sim_ser()
+  fit1 <- with(sim, fit_glm_ser(X, y, prior_variance = 1))
+  fit2 <- with(sim, fit_glm_ser2(X, y, prior_variance = 1))
+})
