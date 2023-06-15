4a5d6aa6d7cb70bc5290f6d1e4cd37493f14f7b3
Karl Tayeb <karl.tayeb7@gmail.com>
2023-06-15 10:10:22 -0400

    implement estimate_prior_variance by optimizing asymptotic BF for SER

--- R/glm_ser.R
+++ R/glm_ser.R
@@ -1,5 +1,36 @@
 # Adapted from: https://andrewg3311.github.io/susieR_logistic_wflow/susie_logistic_demonstration.html#adjustments_to_susie_code
 
+#' Compute log of the asymptotic Bayes factor (ABF) for the SER vs null model
+#' @param betahat vector of MLEs for the effect size
+#' @param s2 vector of standard errors
+#' @param prior_variance variance of the effect size b ~ N(0, prior_variance)
+#' @param pi prior over non-zero effects
+#' @returns log of the ABF for the SER log(\sum_j \pi_j ABF_j)
+compute_log_abf_ser <- function(betahat, s2, prior_variance, pi){
+  log_abf <- dnorm(betahat, 0, sd = sqrt(s2 + prior_variance), log = T) -
+    dnorm(betahat, 0, sd = sqrt(s2), log = T)
+  return(logSumExp(log_abf + log(pi)))
+}
+
+
+#' Optimize prior variance
+#'
+#' Optimize the asymptotic BF for the SER w.r.t the prior variance
+#' @param betahat vector of MLEs for the effect size
+#' @param s2 vector of standard errors
+#' @param pi prior over non-zero effects
+#' @returns the optimimum prior variance
+optimize_prior_variance <- function(betahat, s2, pi){
+  f_opt <- function(prior_variance){
+    compute_log_abf_ser(betahat, s2, prior_variance, pi)
+  }
+
+  # TODO: reasonable upper bound?
+  upper <- max(betahat^2 + 10 * s2)
+  opt <- optimise(f_opt, c(0, upper), maximum = T)$maximum
+  return(opt)
+}
+
 # compute SER using GLM
 #' @export
 fit_glm_ser <- function(X, y, o = NULL,
@@ -76,30 +107,34 @@ fit_glm_ser <- function(X, y, o = NULL,
   return(res)
 }
 
-#' compute SER using GLM, use Laplace approximation to BF rather than ABF
-#' @export
-fit_glm_ser2 <- function(X, y, o = NULL,
-                         prior_variance = 1, intercept = T,
-                         prior_weights = NULL, family = "binomial") {
-  estimate_intercept <- intercept # hack
-
+#' Map univariate regression
+#'
+#' Fit univariate glm for each column of X
+#' @param X n x p design matrix
+#' @param y n vector response
+#' @param o optional offset, vector of length n
+#' @param estimate_intercept boolean to fit intercept
+#' @param family family for glm, e.g. binomial
+#' @returns list containing intercept and effect estimates,
+#'    standard errors, and likelihood ratios
+map_univariate_regression <- function(X, y, off = NULL, estimate_intercept=T, family='binomial'){
   p <- ncol(X)
   betahat <- numeric(p)
   shat2 <- numeric(p)
   intercept <- rep(0, p)
   lr <- rep(0, p)
 
-  if (is.null(o)) {
-    o <- rep(0, length(y))
+  if (is.null(off)) {
+    off <- rep(0, length(y))
   }
 
   for (j in 1:p) {
     if (estimate_intercept) {
-      log.fit <- glm(y ~ X[, j] + 1 + offset(o), family = family)
+      log.fit <- glm(y ~ X[, j] + 1 + offset(off), family = family)
       intercept[j] <- unname(coef(log.fit)[1])
     }
     else {
-      log.fit <- glm(y ~ X[, j] - 1 + offset(o), family = family)
+      log.fit <- glm(y ~ X[, j] - 1 + offset(off), family = family)
     }
     log.fit.coef <- summary(log.fit)$coefficients
     betahat[j] <- ifelse(is.na(log.fit.coef[1 + estimate_intercept,
@@ -109,21 +144,58 @@ fit_glm_ser2 <- function(X, y, o = NULL,
     lr[j] <- 0.5 * (log.fit$null.deviance - log.fit$deviance)
   }
 
+  res <- list(
+    p = p,
+    betahat = betahat,
+    shat2 = shat2,
+    intercept = intercept,
+    lr = lr
+  )
+  return(res)
+}
+
+#' compute SER using GLM, use Laplace approximation to BF rather than ABF
+#' @export
+fit_glm_ser2 <- function(X, y, o = NULL,
+                         prior_variance = 1, intercept = T,
+                         prior_weights = NULL, family = "binomial",
+                         laplace=T, estimate_prior_variance=T) {
+  estimate_intercept <- intercept # hack
+
+  # univariate regressions
+  uni <- map_univariate_regression(X, y, o, intercept, family)
+  lr <- uni$lr
+  betahat <- uni$betahat
+  shat2 <- uni$shat2
+  intercept <- uni$intercept
+  p <- ncol(X)
+
+  # default pi
+  if (is.null(prior_weights)) {
+    prior_weights <- rep(1/p, p)
+  }
+
+  # estimate prior variance
+  if(estimate_prior_variance){
+    prior_variance <- optimize_prior_variance(betahat, shat2, prior_weights)
+  }
+
   # compute ABF
   lbf <- dnorm(betahat, 0, sqrt(prior_variance + shat2), log = TRUE) -
     dnorm(betahat, 0, sqrt(shat2), log = TRUE)
 
-  # compute liklihood ratio under asymptotic approximation
-  alr_mle <- dnorm(betahat, betahat, sqrt(shat2), log=T) -
-    dnorm(betahat, 0, sqrt(shat2), log=T)
+  # make corrected ABF
+  if(laplace){
+    # compute liklihood ratio under asymptotic approximation
+    alr_mle <- dnorm(betahat, betahat, sqrt(shat2), log=T) -
+      dnorm(betahat, 0, sqrt(shat2), log=T)
 
-  # corrected ABF
-  lbf <- lbf - alr_mle + lr
-
-  lbf[is.infinite(shat2)] <- 0
-  if (is.null(prior_weights)) {
-    prior_weights <- rep(1/p, p)
+    lbf <- lbf - alr_mle + lr
   }
+
+  # deal with perfectly enriched/depleted
+  lbf[is.infinite(shat2)] <- 0
+
   maxlbf <- max(lbf)
   w <- exp(lbf - maxlbf)
   w_weighted <- w * prior_weights
