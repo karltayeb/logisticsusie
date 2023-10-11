70968549101889db1401df4c0058afd7a33878e2
Karl Tayeb <karl.tayeb7@gmail.com>
2023-10-11 10:58:07 -0500

    document

---
+++ tests/testthat/test-generalized_ibss_quad.R
@@ -0,0 +1,7 @@
+test_that("multiplication works", {
+  sim <- sim_ser()
+  fit0 <- with(sim, generalized_ibss(X, y, L=1))
+  fit1 <- with(sim, generalized_ibss_quad(X, y, L=1, maxit=1, verbose=F))
+  fit2 <- with(sim, generalized_ibss_quad(X, y, L=1, maxit=1, verbose=F, n=1024))
+  plot(fit$fits[[1]]$lbf, fit0$fits[[1]]$lbf); abline(0, 1, col='red')
+})
